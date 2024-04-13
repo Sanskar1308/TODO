38118df9b9d689f56f647966c1cc6d82b3c2664d
Sanskar Chirania <sanskar125112@gmail.com>
2024-04-13 12:35:40 +0530

    TODO created

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/todo", async (req, res) => {
+    const todos = await todo.find({});
+
+    res.json({
+        todos
+    })
+});
+
+app.post("/todo", async (req, res) => {
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if (!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json ({
+        msg: "Todo created"
+    })
+});
+
+app.put("/completed",async (req, res) => {
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
+    if (!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    await todo.updateOne({
+        _id: req.body.id
+    },{
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as completed"
+    })
+});
+
+app.delete("/todo", (req, res) => {
+
+});
+
+app.listen(3000);
